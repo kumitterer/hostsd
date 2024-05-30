214c95dbad11e88b3f51e04eb2d7b0fa06b8d6a2
Kumi <git@kumi.email>
2024-05-30 08:36:52 +0200

    feat: enhance hosts file generation
    Refactored the hosts file generation logic to improve its functionality and readability. Introduced a more robust file reading system that recursively reads all text files in a given directory, excluding ones starting with a period, ending with `.disabled`, or containing the `HOSTSD IGNORE` marker. This enhancement helps in accommodating a broader range of file organizations and simplifying the exclusion of specific files from the hosts file generation process. Additionally, updated the version to 0.1.1 to mark this incremental improvement.
    
    - Improved file reading by handling nested directories and providing options to ignore certain files.
    - Added capability to prefix file paths in the generated content, enhancing traceability of file origins.
    - Streamlined error handling for non-text files and introduced informative skip messages for ignored files or binary content.

--- src/hostsd/__main__.py
+++ src/hostsd/__main__.py
@@ -1,8 +1,7 @@
 from pathlib import Path
-from os import PathLike
-from typing import Union
+from os import PathLike, sep
+from typing import Union, Optional, List
 from argparse import ArgumentParser
-from pathlib import Path
 
 import sys
 
@@ -11,6 +10,51 @@ HOSTS_FILE = "/etc/hosts"
 
 HOSTS_DIR_WINDOWS = "C:\\Windows\\System32\\drivers\\etc\\hosts.d\\"
 HOSTS_FILE_WINDOWS = "C:\\Windows\\System32\\drivers\\etc\\hosts"
+
+
+def read_directory(
+    dirpath: Optional[Union[str, PathLike]] = HOSTS_DIR, prefix: Optional[str] = None
+) -> List[Path]:
+    """Read all files from a directory
+
+    This will not read files with names that begin with a period ("."), or binary files.
+
+    Args:
+        dir (str, optional): The directory from which to read files. Defaults to "/etc/hosts.d/".
+        prefix (str, optional): Prefix to add to the file path in the output. Defaults to None.
+
+    Raises:
+        IOError: Raised if the provided source path does not exist or is not a directory.
+
+    Returns:
+        list[Path]: List of Path objects representing files in the directory
+    """
+    directory = Path(dirpath)
+
+    if not (directory.exists() and directory.is_dir()):
+        raise IOError(f"Directory {dirpath} does not exist or is not a directory.")
+
+    for infile in sorted(list(directory.glob("**/*")), key=lambda f: f.name):
+        full_path = f"{prefix if prefix else ''}{sep if prefix and prefix[-1] != sep else ''}{infile.name}"
+
+        if (
+            infile.is_file()
+            and not infile.name.startswith(".")
+            and not infile.name.endswith(".disabled")
+        ):
+            try:
+                filecontent = infile.read_text()
+                if "HOSTSD_IGNORE" in filecontent:
+                    print(f"File {infile.name} is marked as ignored - skipping")
+                    continue
+            except UnicodeDecodeError:
+                print(f"File {infile.name} is not a text file - skipping")
+                return
+
+            yield f"# {full_path}\n\n{filecontent}"
+
+        elif infile.is_dir():
+            yield from read_directory(infile, full_path)
 
 
 def get_new_content(dirpath: Union[str, PathLike] = HOSTS_DIR) -> str:
@@ -34,22 +78,11 @@ def get_new_content(dirpath: Union[str, PathLike] = HOSTS_DIR) -> str:
     if not (directory.exists() and directory.is_dir()):
         raise IOError(f"Directory {dirpath} does not exist or is not a directory.")
 
-    for infile in sorted(list(directory.iterdir()), key=lambda f: f.name):
-        if (
-            infile.is_file()
-            and not infile.name.startswith(".")
-            and not infile.name.endswith(".disabled")
-        ):
-            with infile.open('r') as openfile:
-                try:
-                    filecontent = openfile.read()
-                except UnicodeDecodeError:
-                    print(f"File {infile.name} is not a text file - skipping")
+    for infile in read_directory(dirpath):
+        if content:
+            content += "\n\n"
 
-                if content:
-                    content += "\n\n"
-
-                content += f"# {infile.name}\n\n{filecontent}"
+        content += infile
 
     return content
 
@@ -64,7 +97,7 @@ def write_hosts_file(content: str, path: Union[str, PathLike] = HOSTS_FILE):
     Raises:
         IOError: Raised if the provided file path could not be written to.
     """
-    with Path(path).open('w') as hostsfile:
+    with Path(path).open("w") as hostsfile:
         try:
             hostsfile.write(content)
         except Exception as e:
